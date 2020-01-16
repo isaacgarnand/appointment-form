25ccf5551715ab672067fc05dee582aad65d7664
Isaac Garnand and Sommer Howe <isaac@>
2020-01-15 16:27:52 -0800

    got the button working

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 $(function(){    
-
-  $("form#urname").click(function(event){
+  $("form#urname").submit(function(event){
     event.preventDefault();
     var inputName = $("#name").val();
     var inputDesc = $("#desc").val();
     var inputDate = $("#date").val(); 
     var timeThru = $("#thru").val();
-
   $("#output").text(inputName); 
   $("#output2").text(inputDesc);
   $("#output3").text(inputDate);
   $("#output4").text(timeThru);
 });
 });
+
