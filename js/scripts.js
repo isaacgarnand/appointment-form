d915dc3e650d658caea31fb7353ccfa6a74531d2
Isaac Garnand and Sommer Howe <sommer@>
2020-01-15 16:16:38 -0800

    made form you can fill out and hit the enter key on keybord

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(function(){    
+
+  $("form#urname").click(function(event){
+    event.preventDefault();
+    var inputName = $("#name").val();
+    var inputDesc = $("#desc").val();
+    var inputDate = $("#date").val(); 
+    var timeThru = $("#thru").val();
+
+  $("#output").text(inputName); 
+  $("#output2").text(inputDesc);
+  $("#output3").text(inputDate);
+  $("#output4").text(timeThru);
+});
+});
